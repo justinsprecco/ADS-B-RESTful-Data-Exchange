105e9d374a8ec19ac4f7ad1ae75c0572d91586ea
Nate Shaw <nathaniel.shaw@wsu.edu>
2024-11-19 03:08:20 +0000

    Update Schemas

--- brokerService/src/database/mongodb_init.js
+++ brokerService/src/database/mongodb_init.js
@@ -1,6 +1,25 @@
 var mongoose = require('mongoose')
 var Schema = mongoose.Schema
 
+
+/*
+The user schema contains the id of a user and the number of 
+landings/touchandgo during its entire lifetime for all of its groundstations.
+
+numLandings contain the total number of landings (not including touch and go)
+for a user.
+
+touchAndGo specifically contain the total number of "touch and go" for a
+user.
+*/
+const userSchema = new Schema(
+{
+	_id: {type: Number, uniquer: true},
+	numLandings: Number,
+	touchAndGo: Number
+}
+)
+
 /*
 The message schema stores the message contents along with the groundstation
 ID that corresponds to the message.
@@ -8,7 +27,22 @@ ID that corresponds to the message.
 A timestamp is given to the message so that it can be determined if the
 message is over a week old and needs to be purged.
 */ 
-const messageSchema = new Schema(
+const messageADSBSchema = new Schema(
+{
+	_id: {type: Number, unique: true},
+	data: String,
+	timestamp: { type: Date, default: Date.now },
+	groundstationID: Number
+})
+
+/*
+The message Radar schema stores the message contents along with the 
+groundstation ID that corresponds to the message.
+
+A timestamp is given to the message so that it can be determined if the
+message is over a week old and needs to be purged.
+*/ 
+const messageRadarSchema = new Schema(
 {
 	_id: {type: Number, unique: true},
 	data: String,
@@ -18,13 +52,14 @@ const messageSchema = new Schema(
 
 /*
 Groundstation schema relies on the client to have its _id field match up
-with the groundstationID key from the messageSchema.
+with the groundstationID key from the messageADBSSchema and 
+messageRadarSchema.
 
-numLandings contain the total number of landings (including touch and go)
-for a groundstation for the entire lifetime.
+Groundstation schema also relies on the client to have its userID field
+match up with userSchema's _id field.
 
-touchAndGo specifically contain the total number of "touch and go" for a
-groundstation for the entire lifetime.
+A user can have many groundstations but a groundstation can only have one
+user.
 */
 const groundstationSchema = new Schema(
 {
@@ -32,6 +67,5 @@ const groundstationSchema = new Schema(
 	macAddress: {type: String, unique: true},
 	latitude: Number,
 	longitude: Number,
-	numLandings: Number,
-	touchAndGo: Number
+	userID: Number
 })
